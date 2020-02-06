084d3c216e5b0c3232ec2d35a23aea54997cae2d
Taylor Sahli <taylorsahl@mail.weber.edu>
2020-02-05 17:25:38 -0700

    added existing files from assignment 1 & 2

---
+++ BlabberApp.Domain/IDataStore.cs
@@ -0,0 +1,10 @@
+namespace BlabberApp.Domain
+{
+    public interface IDataStore
+    {
+        bool Create(IDatum datum);
+        IDatum Read(ISpecimen specimen);
+        bool Update(IDatum datum);
+        bool Delete(ISpecimen specimen);
+    }
+}
